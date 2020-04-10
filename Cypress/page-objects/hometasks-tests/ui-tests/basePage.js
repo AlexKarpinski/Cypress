504c0eb027d76d8f5dc19c17d23378f44d6e80e6
AlexKarpinski <raptoooor@mail.ru>
2020-04-10 15:08:46 +0300

    auto

---
+++ Cypress/page-objects/hometasks-tests/ui-tests/basePage.js
@@ -0,0 +1,20 @@
+class BasePage {
+    open() {
+        cy.visit('https://store.google.com/us/collection/accessories_wall?hl=en-US');
+    }
+
+    get searchIcon() {
+        return cy.get('.header-search-icon');
+    }
+
+    get searchInput() {
+        return cy.get('input[aria-label = "Search Google Store"]');
+    }
+
+    performSearch(productToSearch) {
+            this.searchIcon.click();
+            this.searchInput.type(`${productToSearch}{enter}`);
+    }
+}
+
+export default new BasePage()
