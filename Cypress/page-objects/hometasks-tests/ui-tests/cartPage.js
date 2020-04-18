7d34a8b5948194bb398665690aa6f506326c5f39
AlexKarpinski <raptoooor@mail.ru>
2020-04-18 17:30:45 +0300

    add testREQ02.js in auto-tests

--- Cypress/page-objects/hometasks-tests/ui-tests/cartPage.js
+++ Cypress/page-objects/hometasks-tests/ui-tests/cartPage.js
@@ -1,5 +1,9 @@
 class CartPage {
 
+    open() {
+        cy.visit('https://store.google.com/us/cart?hl=en-US');
+    }
+
     getProductName() {
         return cy.get('a > .roboto-header-text-9')
     }
@@ -8,8 +12,24 @@ class CartPage {
         return cy.get('.text-right')
     }
 
+    getProductName(product) {
+        return cy.get(`[data-backend-docid='${product.docId}'] > .row`)
+    }
+
+    getProductPrice(product) {
+        return cy.get('.text-right').contains(`${product.price}`)
+    }
+
     getProductCount() {
         return cy.get('.navbar-filler > .visible', {timeout: 60000})
+    }
+
+    changeProductCount(product) {
+        return cy.get(`[data-backend-docid='${product.docId}'] div > .item-qty-selector`, {timeout: 60000}).select('2')
+    }
+
+    getResultPrice() {
+        return cy.get('.float-right')
     }
 
     removeCart() {
--- Cypress/page-objects/hometasks-tests/ui-tests/searchResultsPage.js
+++ Cypress/page-objects/hometasks-tests/ui-tests/searchResultsPage.js
@@ -1,6 +1,7 @@
 class SearchResultsPage {
     clickProductByDocId(docId) {
         return cy.get(`a[href="/product/${docId}"]`).click({force: true});
+        //cy.get('.row').find(`a[href="/product/${docId}"]`).click({force: true});
     }
 }
 
